4e1be4328b11f467f48e45f48b92e2fe086ec901
lucas-espi <lucass.espi@gmail.com>
2023-06-01 20:54:05 -0300

    actualizando 1.49

--- entrega-4/src/app.js
+++ entrega-4/src/app.js
@@ -11,10 +11,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
-app.set('view', `${__dirname}/views`);
+app.set('views', `${__dirname}/views`);
+
+app.use('/', viewsRouter);
 
 
 
 const server = app.listen(8080, () => { console.log(`Run server 8080`)});
 
 const io = new Server(server);
+
+// io.on('connection', socket =>{
+//     console.log('New Client connection');
+
+//     socket.on('message', data=>{
+//         console.log(data);
+//     });
+
+//     socket.emit('evento_socket_individual', 'este mensaje solo lo resive el socket');
+
+//     socket.broadcast.emit('evento_todos_menos_actual', 'Lo van a ver todos los clientes menos el actual')
+
+//     io.emit('evento_todos', 'Todos reciben el mensaje')
+// });
+
+ 
--- entrega-4/src/public/js/index.js
+++ entrega-4/src/public/js/index.js
@@ -1,2 +1,15 @@
 const socket = io();
 
+socket.emit('message', 'Hola a todos esto es un mensaje del front');
+
+socket.on('evento_socket_individual', data=>{
+    console.log(data);
+});
+
+socket.on('evento_todos_menos_actual', data=>{
+    console.log(data);
+});
+
+socket.on('evento_todos', data=>{
+    console.log(data);
+});
